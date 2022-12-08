47fd1b558901869715b7ca3ca29fa2c7e02de24c
ama-rah <aemysolomon@gmail.com>
2022-12-08 15:13:59 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,23 +1,13 @@
 #include <stdio.h>
 
 /**
+ * main - Entry point
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     * Used puts instead of printf
- *
- *      */
-
+ * Return: Always 0 (Success)
+ * Used puts instead of printf
+ * */
 int main(void)
-
 {
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
