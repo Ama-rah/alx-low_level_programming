507540b2b3f270132f4f712a89a518a40f07d84a
ama-rah <aemysolomon@gmail.com>
2022-12-08 16:21:45 -0800

    Quotes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,8 +3,11 @@
 /**
  * main - Prints out the last part of a quote in the standard error
  *
+ *
+ *
  * Return: This time we return an error 1.
  */
+
 int main(void)
 {
 	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
