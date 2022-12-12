123605b4891dc0b9b5a8e57d7d510dde97ecd30e
ama-rah <aemysolomon@gmail.com>
2022-12-12 05:35:06 -0800

    Fixed

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,22 +2,23 @@
 
 /**
  * main - a program that prints the alphabet in lowercase,
- * 	and then in uppercase, followed by a new line.
+ *	and then in uppercase, followed by a new line.
  *
- * 	Return: Always 0
+ *Return: Always 0
  */
+
 int main(void)
 {
-	int ch;
+	int alph;
 
-	for (ch = 97; ch <= 122; ch++)
+	for (alph = 97; alph <= 122; alph++)
 	{
-		for (ch = 97; ch <= 122; ch++)
+		putchar(alph);
 	}
-	for (ch = 65; ch <= 90; ch++)
+	for (alph = 65; alph <= 90; alph++)
 	{
-		putchar(ch);
+		putchar(alph);
 	}
 	putchar('\n');
-	return (0)
+	return (0);
 }
