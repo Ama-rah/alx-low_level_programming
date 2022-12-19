40806bf61af934aa5ff36176170ad274412fc0b9
ama-rah <aemysolomon@gmail.com>
2022-12-19 08:40:36 -0800

    In computer class, the first assignment was to write a program to print the first 100 Fibonacci numbers.

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Computes the sum of all
+ *	the multiples of 3 or 5 below 1024 (excluded)
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	long int fibonacci[50];
+
+	fibonacci[0] = 1;
+	fibonacci[1] = 2;
+	printf("%ld, %ld, ", fibonacci[0], fibonacci[1]);
+
+	for (i = 2; i < 50; i++)
+	{
+		fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+		if (i == 49)
+			printf("%ld\n", fibonacci[i]);
+		else
+			printf("%ld, ", fibonacci[i]);
+	}
+	return (0);
+}
