df3dc6744e02aabc93b7a9f998c658dea653feec
ama-rah <aemysolomon@gmail.com>
2023-01-12 06:15:13 -0800

    _realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -14,7 +14,7 @@
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	char *new_ptr, *temp_ptr;
-	unsigned int;
+	unsigned int i;
 
 	if (new_size == old_size)
 		return (ptr);
