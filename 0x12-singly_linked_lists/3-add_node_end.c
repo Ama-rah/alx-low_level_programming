725bfe0e27c2c070ab1e53a07a5bb538e64f4269
Ama-rah <aemysolomon@gmail.com>
2023-01-27 07:59:14 -0800

    Add node at the end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -31,8 +31,8 @@ list_t *add_node_end(list_t **head, const char *str)
 	new_node->next = NULL;
 
 	/*copy our starting node pointer to temp*/
-	temp = *head
-
+	temp = *head;
+	
 	/*if temp is NULL place our new_node at the beginning of the list*/
 	/*else if its not null we place our new_node to temp->next hence*/
 	/*adding our new_node at the end of the list*/
