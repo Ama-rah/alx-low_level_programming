ccf3b1442badfd62875ad7438e0db23a5930fb4c
Ama-rah <aemysolomon@gmail.com>
2023-02-02 04:12:27 -0800

    Print

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -17,6 +17,8 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	num = ~(num << index);/*bitwise Not operator is an unary operator*/
 	/*which is why we use the () to nest our left shift operator*/
 
+	*n = *n & num;/*bitwise AND operator*/
+
 
 
 	return (1);
